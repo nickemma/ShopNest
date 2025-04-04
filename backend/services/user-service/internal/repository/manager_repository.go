42706b223533c293465742eeab0d6ed982903372
Stark <inventorstark7@gmail.com>
2025-04-04 23:12:02 +0100

    chore(user): renamed manager repository file due to spelling mistake

--- backend/services/user-service/internal/repository/manager_repository.go
+++ backend/services/user-service/internal/repository/manager_repository.go
@@ -11,6 +11,7 @@ import (
 type ManagerRepository interface {
 	RegisterManager(ctx context.Context, manager *domain.RegisterManager, auth *domain.Auth) error
 	GetManagerByEmail(ctx context.Context, email string) error
+	ApproveManager(ctx context.Context, email string) error
 }
 
 type PostgresManagerRepository struct {
@@ -40,10 +41,10 @@ func (mgr *PostgresManagerRepository) RegisterManager(ctx context.Context, manag
 	}
 
 	mgrQuery := `
-		INSERT INTO managers (manager_id, name, email, role)
-		VALUES ($1, $2, $3, $4)`
+		INSERT INTO managers (manager_id, name, email, role, approved)
+		VALUES ($1, $2, $3, $4, $5)`
 
-	_, err = tx.Exec(ctx, mgrQuery, manager.ManagerID, manager.Name, manager.Email, manager.Role)
+	_, err = tx.Exec(ctx, mgrQuery, manager.ManagerID, manager.Name, manager.Email, manager.Role, manager.Approved)
 	if err != nil {
 		return err
 	}
@@ -57,14 +58,22 @@ func (mgr *PostgresManagerRepository) GetManagerByEmail(ctx context.Context, ema
 	manager := &domain.Manager{}
 
 	// Query manager
-	managerQuery := `SELECT manager_id, name, email, role, created_at, updated_at
+	managerQuery := `SELECT manager_id, name, email, role, approved, created_at, updated_at
 					FROM managers WHERE email = $1`
 	err := mgr.db.QueryRow(ctx, managerQuery, email).Scan(
 		&manager.ManagerID, &manager.Name, &manager.Email,
-		&manager.Role, &manager.CreatedAt, &manager.UpdatedAt)
+		&manager.Role, &manager.Approved, &manager.CreatedAt, &manager.UpdatedAt)
 	if err != nil {
 		return nil, err
 	}
 
 	return manager, nil
+}
+
+
+func (mgr *PostgresManagerRepository) ApproveManager(ctx context.Context, email string) error {
+	query := `UPDATE managers SET approved = $1, updated_at = $2 WHERE email = $3`
+	_, err := mgr.db.Exec(ctx, query, "TRUE", time.Now(), email)
+	return err
+
 }
