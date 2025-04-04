29f4ae55769834754f7ae54316f273213e359803
Stark <inventorstark7@gmail.com>
2025-04-04 19:14:35 +0100

    refactore(user:anager): use embedded struct

--- backend/services/user-service/internal/domain/manager_domain.go
+++ backend/services/user-service/internal/domain/manager_domain.go
@@ -4,10 +4,15 @@ import "time"
 
 // Manager represents an admin/support in the system
 type Manager struct {
+	RegisterManager
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+
+type RegisterManager struct {
 	ManagerID string    `json:"managerId"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
 	Roles     string    `json:"roles"` // "Admin" | "Support"
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
 }
